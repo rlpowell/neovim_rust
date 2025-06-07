0aebd140a47c6546714fb2137bb408f2b15ca7e1
Robin Lee Powell <rlpowell@digitalkingdom.org>
2025-06-07 00:08:11 -0700

    Tweaks to the run script

--- run.sh
+++ run.sh
@@ -12,12 +12,17 @@ trap '__error_trapper "${LINENO}/${BASH_LINENO}" "$?" "$BASH_COMMAND"' ERR
 
 set -euE -o pipefail
 
-SRC_DIR="$(pwd)"
-SRC_NAME="$(basename "$(pwd)")"
+# Assume this script is at the other end of a symlink.  Get both
+# ends, call them SRC_DIR (the directory of the source tree the
+# symlink is in) and SCRIPT_DIR (the directory the original script
+# itself is in).
 
-# Assume this script is at the other end of a symlink; go to the
-# directory the script is in
-SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+SRC_DIR="$(realpath "$(dirname "$0")")"
+SRC_NAME="$(basename "$SRC_DIR")"
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+
+# Get to the original directory to build stuff there as needed (it
+# probably isn't)
 cd "$SCRIPT_DIR/"
 
 podman build -t neovim_rust --build-arg USERNAME="$(id -un)" --build-arg UID="$(id -u)" --build-arg GID="$(id -g)" .
@@ -32,20 +37,28 @@ fi
 
 cd "$SRC_DIR/"
 
+CONTAINER_NAME="neovim_rust_${SRC_NAME}"
+
 # Use the source directory's dockerfile if any
-if [[ -f "$SRC_DIR/Dockerfile" ]]
+if [[ -f "$SRC_DIR/Dockerfile.dev" ]]
 then
   IMAGE_NAME=neovim_rust_${SRC_NAME}
-  podman build -t "$IMAGE_NAME" --build-arg USERNAME="$(id -un)" --build-arg UID="$(id -u)" --build-arg GID="$(id -g)" .
+  podman build -t "$IMAGE_NAME" --build-arg USERNAME="$(id -un)" --build-arg UID="$(id -u)" --build-arg GID="$(id -g)" -f "$SRC_DIR/Dockerfile.dev" .
 else
   IMAGE_NAME=neovim_rust
 fi
 
-podman kill "$IMAGE_NAME" 2>/dev/null || true
-podman rm "$IMAGE_NAME" 2>/dev/null || true
+podman kill "$CONTAINER_NAME" 2>/dev/null || true
+podman rm "$CONTAINER_NAME" 2>/dev/null || true
+
+POD_NAME="${CONTAINER_NAME}_pod"
+podman pod stop "$POD_NAME" 2>/dev/null || true
+podman pod rm "$POD_NAME" 2>/dev/null || true
+
+podman pod create --userns=keep-id --network=slirp4netns -p 50555:50555 "$POD_NAME" 2>/dev/null || true
 
 # See https://github.com/xd009642/tarpaulin/issues/1087 for the seccomp thing
-podman run --name "$IMAGE_NAME" --userns=keep-id --security-opt seccomp="$SCRIPT_DIR/seccomp.json" -w "/home/$(id -un)" \
+podman run --name "$CONTAINER_NAME" --pod="$POD_NAME" --security-opt seccomp="$SCRIPT_DIR/seccomp.json" -w "/home/$(id -un)" \
   -e SRC_DIR="$SRC_DIR" \
   -v ~/src:"/home/$(id -un)/src" -v "$LOCAL_DIR:/home/$(id -un)/.cargo" \
   -v ~/config/dotfiles/nvim:"/home/$(id -un)/.config/nvim" -v ~/config/dotfiles/bashrc:"/home/$(id -un)/.bashrc" \
